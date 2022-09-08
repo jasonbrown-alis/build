dcaf2f64c76d886bc48b320d0b762aec027795a0
ruanspies <ruan@alis.capital>
2022-09-08 11:27:44 +0200

    docs: refactor sidebar and navigation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,26 +19,25 @@ export default {
 		markdown:{
 			theme: "material-lighter"
 		},
-		nav: [
-			{
-				text: toTitleCase(ROOT_GUIDES),
-				link: `/${ROOT_GUIDES}/getting-started/introduction`
-			},
-			// {
-			// 	text: toTitleCase(ROOT_SAMPLES),
-			// 	link: `/${ROOT_SAMPLES}/`
-			// },
-			{
-				text: toTitleCase(ROOT_REFERENCE),
-				link: '/references/core-concepts'
-			}
-		],
+		// nav: [
+		// 	{
+		// 		text: toTitleCase(ROOT_GUIDES),
+		// 		link: `/${ROOT_GUIDES}/getting-started/introduction`
+		// 	},
+		// 	// {
+		// 	// 	text: toTitleCase(ROOT_SAMPLES),
+		// 	// 	link: `/${ROOT_SAMPLES}/`
+		// 	// },
+		// 	{
+		// 		text: toTitleCase(ROOT_REFERENCE),
+		// 		link: '/references/core-concepts'
+		// 	}
+		// ],
         socialLinks: [
             { icon: 'github', link: 'https://github.com/alis-exchange/build' },
             { icon: 'youtube', link: 'https://www.youtube.com/channel/UCcUppUH_3cLwHUEJ7e4lLLg/featured' },
         ],
-		sidebar: {
-			'/guides/': [
+		sidebar:  [
 				{
 					text: 'Getting Started',
                     collapsible: true,
@@ -46,80 +45,74 @@ export default {
                         {
                             text: 'Introduction',
                             link: `/${ROOT_GUIDES}/getting-started/introduction`
-                        }
+                        },
+						{
+							text: "Overview",
+							items: [
+								{
+									text: 'Conceptual framework',
+									link: '/references/core-concepts'
+								},
+								{
+									text: 'Developer flow',
+									link: '/guides/build/overview'
+								},
+								{
+									text: 'Consumer experience',
+									link: '/guides/consume/quick-start'
+								},
+							]
+						},
+						{
+							text: 'Install the Alis CLI',
+							link: '/guides/configuration/command-line-interface'
+						},
                     ]
 				},
 				{
 					text: 'How-to-guides',
                     collapsible: true,
 					items: [
-                        {
-                            text: 'Build',
-                            items: [
-                                {
-                                    text: 'Overview',
-                                    link: '/guides/build/overview'
-                                },
-                                {
-                                    text: 'Leveraging auto-generated documentation',
-                                    link: '/guides/build/auto-generated-docs'
-                                }
-                            ]
-                        },
-                        {
-                            text: 'Consume',
-                            items: [
-                                {
-                                    text: 'Quick start',
-                                    link: '/guides/consume/quick-start'
-                                },
-                                {
-                                    text: 'Make a request to a product',
-                                    link: '/guides/consume/make-your-first-request'
-                                },
-                            ]
-                        },
-                        {
-                            text: 'Device Configuration',
-                            items: [
-                                {
-                                    text: 'Install the Alis CLI',
-                                    link: '/guides/configuration/command-line-interface'
-                                },
-                                {
-                                    text: 'Configure your IDE',
-                                    link: '/guides/configuration/set-up-your-favourite-IDE'
-                                },
-                            ]
-                        }
-                    ]
-				}
-			],
-			'/references/':[
-				{
-					text: 'Reference Documents',
-					items: [
-                        {
-                            text: 'Alis Exchange core concepts',
-                            link: '/references/core-concepts'
-                        },
-                        {
-                            text: 'Resource-oriented design',
-                            link: '/references/resource-oriented-design'
-                        },
-                    ],
+						{
+							text: 'Make a request to a product',
+							link: '/guides/consume/make-your-first-request'
+						},
+						{
+							text: 'Leveraging auto-generated documentation',
+							link: '/guides/build/auto-generated-docs'
+						},
+						{
+							text: 'Configure your IDE',
+							link: '/guides/configuration/set-up-your-favourite-IDE'
+						},
+					]
 				},
 				{
 					text: 'Further Reading',
-                    items: [
-                        {
-                            text: "Additional resources",
-                            link: '/references/other-resources/other-resources'
-                        }
-                    ]
+					collapsible: true,
+					items: [
+						{
+							text: 'Resource-oriented design',
+							link: '/references/resource-oriented-design'
+						},
+						{
+							text: "Core technologies",
+							link: '/references/other-resources/other-resources'
+						},
+					]
 				},
-			]
-		},
+			],
+			// '/references/':[
+			// 	{
+			// 		text: 'Reference Documents',
+			// 		items: [
+            //             {
+            //                 text: 'Alis Exchange core concepts',
+            //                 link: '/references/core-concepts'
+            //             },
+            //         ],
+			// 	},
+			// ]
 	}
 }
 
